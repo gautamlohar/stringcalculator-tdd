606ee14374dcef547f19297358869510abe08e26
Gautam Lohar <gautamlohar@Mac.lan>
2025-06-21 22:36:42 +0530

    Updated add function to return same number if only one character is passed and added test case to return the sum of two numbers by add function when the input string contains two values separated by a comma

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const add = (str) => {
     if (str.length === 0) return 0;
+     else return parseInt(str);
 }
 
 module.exports = add;
--- src/index.test.js
+++ src/index.test.js
@@ -7,3 +7,7 @@ test('should return number 0 when empty string is passed add function', () => {
 test('should return same number if only one character is passed to add function', () => {
     expect(add("1")).toBe(1);
 });
+
+test('should return the sum of two numbers by add function when the input string contains two values separated by a comma', () => {
+    expect(add("1,2")).toBe(3);
+});
