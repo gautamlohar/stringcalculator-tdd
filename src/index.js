ceb0b0871468212eee292eb5e8ee4c8531ada69b
Gautam Lohar <gautamlohar@Mac.lan>
2025-06-21 23:27:11 +0530

    Updated add function to throw error on negative values in the string that is passed to add function as argument also modified test case to handle throwing error on negative value

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 
 const add = (str) => {
-  if (!str) return 0;
+    if (!str) return 0;
 
-  let delimiter = /,|\n/;
-  let numbers = str;
+    let delimiter = /,|\n/;
+    let numbers = str;
 
-  if (str.startsWith("//")) {
-    const parts = str.split('\n');
-    delimiter = parts[0][2];
-    numbers = parts[1];
-  }
+    if (str.startsWith("//")) {
+        const parts = str.split('\n');
+        delimiter = parts[0][2];
+        numbers = parts[1];
+    }
 
-  return numbers
-    .split(delimiter)
-    .map(Number)
-    .reduce((sum, num) => sum + num, 0);
+    const nums = numbers.split(delimiter).map(Number);
+    const negativeNos = nums.filter(n => n < 0);
+
+    if (negativeNos.length > 0) {
+        throw new Error(`negative numbers not allowed ${negativeNos.join(', ')}`);
+    }
+
+    const sum = nums.reduce(((sum, num) => sum + num), 0);
+    return sum;
 };
 
 module.exports = add;
--- src/index.test.js
+++ src/index.test.js
@@ -25,5 +25,5 @@ test('should return sum of multiple numbers if string contains multiple chars se
 });
 
 test('should throw an error for negative numbers if passed to add function', () => {
-    expect(add("1,-2")).toThrow('negative numbers not allowed -2');
+    expect(() => add("1,-2")).toThrow('negative numbers not allowed -2');
 });
